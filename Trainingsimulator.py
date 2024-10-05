7a0a001633ced9a93cc590999de83d7d70a695d0
SWAPNIL PATIL <swapnilpatil020198@gmail.com>
2024-10-05 14:45:07 +0530

    test

--- Trainingsimulator.py
+++ Trainingsimulator.py
@@ -1,6 +1,18 @@
 from utils import *
+from sklearn.model_selection import train_test_split
 
 ##Step 1
 
 path='mydata'
 data=importdatainfo(path)
+
+#Step 2
+data=balanceData(data, display=False)
+
+#Step 3
+imagesPath, steerings = loadData(path,data)
+
+#Step 4
+xTrain, xVal, yTrain, yVal = train_test_split(imagesPath, steerings, test_size=0.2,random_state=10)
+print('Total Training Images: ',len(xTrain))
+print('Total Validation Images: ',len(xVal))
--- utils.py
+++ utils.py
@@ -1,13 +1,92 @@
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import image as mpimg
 import os
-
+from sklearn.utils import shuffle
+import cv2
+import numpy as np
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import cv2
+import imgaug
 def getName(filepath):
     return filepath.split('\\')[-1]
 
 
 def importdatainfo(path):
-    columns=['Center', 'Left','Right', 'Sterring', 'Throttle', 'Brake','Speed']
+    columns=['Center', 'Left','Right', 'Steering', 'Throttle', 'Brake','Speed']
     data=pd.read_csv(os.path.join(path,'driving_log.csv'),names=columns)
     data['Center']=data['Center'].apply(getName)
     print('Total Images Imported:',data.shape[0])
+
+    return data
+
+
+def balanceData(data,display=True):
+    nBin = 31
+    samplesPerBin = 500
+    hist, bins = np.histogram(data['Steering'], nBin)
+    if display:
+        center = (bins[:-1] + bins[1:]) * 0.5
+        plt.bar(center, hist, width=0.06)
+        plt.plot((np.min(data['Steering']), np.max(data['Steering'])), (samplesPerBin, samplesPerBin))
+        plt.show()
+
+
+    removeindexList = []
+    for j in range(nBin):
+        binDataList = []
+        for i in range(len(data['Steering'])):
+            if data['Steering'][i] >= bins[j] and data['Steering'][i] <= bins[j + 1]:
+                binDataList.append(i)
+        binDataList = shuffle(binDataList)
+        binDataList = binDataList[samplesPerBin:]
+        removeindexList.extend(binDataList)
+
+    print('Removed Images:', len(removeindexList))
+    data.drop(data.index[removeindexList], inplace=True)
+    print('Remaining Images:', len(data))
+
+
+    if display:
+        hist, _ = np.histogram(data['Steering'], (nBin))
+        plt.bar(center, hist, width=0.06)
+        plt.plot((np.min(data['Steering']), np.max(data['Steering'])), (samplesPerBin, samplesPerBin))
+        plt.show()
+
+    return data
+
+
+def loadData(path, data):
+  imagesPath = []
+  steering = []
+  for i in range(len(data)):
+    indexed_data = data.iloc[i]
+    imagesPath.append(f'{path}/IMG/{indexed_data[0]}')
+    steering.append(float(indexed_data[3]))
+  imagesPath = np.asarray(imagesPath)
+  steering = np.asarray(steering)
+  return imagesPath, steering
+
+
+def augmentImage(imgPath,steering):
+    img =  mpimg.imread(imgPath)
+    if np.random.rand() < 0.5:
+        pan = iaa.Affine(translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)})
+        img = pan.augment_image(img)
+    if np.random.rand() < 0.5:
+        zoom = iaa.Affine(scale=(1, 1.2))
+        img = zoom.augment_image(img)
+    if np.random.rand() < 0.5:
+        brightness = iaa.Multiply((0.2, 1.2))
+        img = brightness.augment_image(img)
+    if np.random.rand() < 0.5:
+        img = cv2.flip(img, 1)
+        steering = -steering
+    return img, steering
+
+
+imaRe,st= augmentImage('test.jpg',0)
+plt.imshow(imaRe)
+plt.show()
